c1b085471e919b37c4dc7527eff8c4c0261b3bfb
Martin Kierkegaard <mark@easj.dk>
2017-08-16 13:13:55 +0200

    tilføjet ny controller HalloController

---
+++ Test_CI_Webservice/Controllers/HalloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Test_CI_Webservice.Controllers
+{
+    public class HalloController : ApiController
+    {
+
+        public string Get()
+        {
+            return "Hallo EASJ";
+
+        }
+
+    }
+}
